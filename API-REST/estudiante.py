ccef50ba4080ec0ab2ecae9e2319a31d517e0144
Laura <88988055+Lauralx@users.noreply.github.com>
2022-10-27 19:42:33 -0500

    Add files via upload

---
+++ API-REST/estudiante.py
@@ -0,0 +1,6 @@
+estudiante = [
+    { "codigo": "2182000", "name": "Gu Tai", "nivel": 1, "programacion": 1, "lang":"EN" },
+    { "codigo": "2182001", "name": "Yi Ran", "nivel": 4, "programacion": 1, "lang":"EN" },
+    { "codigo": "2182002", "name": "Lin Lengyu", "nivel": 2, "programacion": 1, "lang":"EN" },
+    { "codigo": "2182003", "name": "Yuo Juan", "nivel": 5, "programacion": 1, "lang":"EN" },
+]
