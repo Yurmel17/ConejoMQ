070ffb0b6e61cbff662f47ccae336c12380a050a
Laura <88988055+Lauralx@users.noreply.github.com>
2022-12-06 23:20:41 -0500

    Add files via upload

---
+++ API Redis - Compose/app.py
@@ -0,0 +1,37 @@
+from itertools import product
+from flask import Flask,jsonify, request
+
+app = Flask(__name__)
+
+from estudiante import estudiante
+
+@app.route('/inicio')
+def inicio():
+    return jsonify({ "message": "Bienvenido"})
+
+@app.route( '/demo')
+def getEstudiantes():
+    return jsonify({"estudiantes": estudiante, "message": "Estudent List's"})
+
+#, methods=['GET']
+@app.route( '/demo/greeting/<string:estudiante_codigo>', methods=['GET'] )
+def getEstudiante(estudiante_codigo):
+    estudianteFound = [estudiante for estudiante in estudiante if estudiante["codigo"]== estudiante_codigo ]
+    if (len(estudianteFound) > 0 ):
+        return jsonify({ "Datos_Estudiante": estudianteFound[0] })
+    return jsonify({ "message": "Estudiante not Found"})
+
+@app.route('/demo/register', methods=['POST'])
+def addEstudiante():
+    new_estudiante = {
+        "codigo": request.json['codigo'],
+        "name": request.json['name'],
+        "email": request.json['email'],
+        "carrera": request.json['carrera'],
+        "nivel": request.json["nivel"]
+    }
+    estudiante.append(new_estudiante)
+    return jsonify({"message": "Estudiante agregado Existosamente", "estudiante": estudiante})
+
+if __name__=='__main__':
+    app.run(debug=True, port=4000)
---
+++ API Redis - Compose/estudiante.py
@@ -0,0 +1,6 @@
+estudiante = [
+    { "codigo": "2182000", "name": "Gu Tai", "email": "gulo@gmail.com", "carrera": "ingenieria de sistemas", "nivel":"1" },
+    { "codigo": "2182001", "name": "Yi Ran", "email": "yira@gmail.com", "carrera": "ingenieria de sistemas", "nivel":"2" },
+    { "codigo": "2182002", "name": "Lin Leng","email": "lin@gmail.com", "carrera": "ingenieria de sistemas", "nivel":"3" },
+    { "codigo": "2182003", "name": "Yuo Juan", "email": "yuo@gmail.com", "carrera": "ingenieria de sistemas", "nivel":"5"},
+]
